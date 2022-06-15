448c79acbc910016768795f47013420a7bae8a43
Linmo <30459943+MoorCorPa@users.noreply.github.com>
2022-06-16 00:15:09 +0800

    rebuild

---
+++ src/test/java/com/work/manager/ManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.work.manager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ManagerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
