3150db48dab897a2ee4eb8ea056d79a587c3a803
Linmo <30459943+MoorCorPa@users.noreply.github.com>
2022-06-23 16:57:39 +0800

    Update UploadController.java

--- src/main/java/com/work/controller/UploadController.java
+++ src/main/java/com/work/controller/UploadController.java
@@ -2,9 +2,12 @@ package com.work.controller;
 
 import com.work.utils.Message;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
 import java.io.File;
 import java.util.UUID;
 
@@ -12,8 +15,9 @@ import java.util.UUID;
 public class UploadController {
 
     @PostMapping("/upload")
-    public Object upload(MultipartFile file){
+    public Object upload(HttpServletRequest request, @RequestParam("file") MultipartFile file){
 
+        HttpSession session = request.getSession();
         String fileName = file.getOriginalFilename();
         String suffixName = fileName.substring(fileName.lastIndexOf("."));
         fileName = UUID.randomUUID()+suffixName;
@@ -21,6 +25,7 @@ public class UploadController {
         try {
             //将图片保存到static文件夹里
             file.transferTo(new File(filePath+fileName));
+            session.setAttribute("filePath", filePath+fileName);
             //返回提示信息
             return new Message(0, fileName);
         } catch (Exception e) {
